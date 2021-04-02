6ae9d587e07fb5bf1ee325b0df2b2ffff7beecd9
Khizar Murad <123hashinclude@gmail.com>
2021-04-02 19:42:35 +0500

    added basic component and library for calendar

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    PostEditComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent, pathMatch: 'full' }])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
