3d5b7ded1e4204c816c329fbe7991316923081b0
Khizar Murad <123hashinclude@gmail.com>
2021-04-04 21:54:32 +0500

    dashboard component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './components/post-create/post-create.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     PostEditComponent,
-    PostListComponent
+    PostListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
