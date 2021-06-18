54fbbd7a1a335087559cc0c541e08b8bba23ff7c
Khizar Murad <123hashinclude@gmail.com>
2021-06-18 21:10:58 +0500

    migrate css to sass

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { CalendarOptions } from '@fullcalendar/angular'; // useful for typecheck
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   calendarOptions: CalendarOptions = {
