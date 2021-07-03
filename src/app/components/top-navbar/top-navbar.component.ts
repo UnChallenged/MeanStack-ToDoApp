c73ea06d23d4a88b99a5ba19b2ffc6b832de69b5
Khizar Murad <123hashinclude@gmail.com>
2021-07-03 22:18:35 +0500

    need to fix changes

--- src/app/components/top-navbar/top-navbar.component.ts
+++ src/app/components/top-navbar/top-navbar.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-
+import { SidenavService } from 'src/app/Services/sidenav.service';
+interface Page {
+  link: string;
+  name: string;
+  icon: string;
+}
 @Component({
   selector: 'app-top-navbar',
   templateUrl: './top-navbar.component.html',
@@ -9,11 +14,35 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class TopNavbarComponent implements OnInit {
   @Input() sidenav!: MatSidenav;
 
-  constructor() {
-   
+  public sideNavState: boolean = false;
+  public linkText: boolean = false;
+  public onSideNavChange: boolean=false;
+  
+  public pages: Page[] = [
+    {name: 'Inbox', link:'some-link', icon: 'inbox'},
+    {name: 'Starred', link:'some-link', icon: 'star'},
+    {name: 'Send email', link:'some-link', icon: 'send'},
+  ]
+
+  constructor(private _sidenavService: SidenavService) {
+    
+    this._sidenavService.sideNavState$.subscribe( res => {
+      console.log(res)
+      this.onSideNavChange = res;
+    })
+
    }
 
   ngOnInit(): void {
+  }
+
+  onSinenavToggle() {
+    this.sideNavState = !this.sideNavState
+    
+    setTimeout(() => {
+      this.linkText = this.sideNavState;
+    }, 200)
+    this._sidenavService.sideNavState$.next(this.sideNavState)
   }
 
 }
